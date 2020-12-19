d3f3e1ac8755376c9afb6268fe48bb93edf2df09
adityakrishana <70339318+adityakrishana@users.noreply.github.com>
2020-12-19 18:13:08 -0500

    ADITYA

---
+++ Model.cs
@@ -0,0 +1,27 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Cars
+{
+    public class Model
+    {
+        public String name;
+        public int imageID;
+
+
+        public Model(String nameInfo, int imageInfo)
+        {
+            this.name = nameInfo;
+            this.imageID = imageInfo;
+        }
+
+    }
+}
