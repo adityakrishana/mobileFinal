2ba2dc939eeea40af225c320a338f031cd8c466f
mandy99999999999 <55302000+mandy99999999999@users.noreply.github.com>
2020-12-19 18:26:59 -0500

    Mandeep

---
+++ Member.cs
@@ -0,0 +1,24 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Realms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Cars
+{
+    public class Member : RealmObject
+    {
+        public string email { get; set; }
+        public string name { get; set; }
+        public string password { get; set; }
+        public string number { get; set; }
+        public string age { get; set; }
+
+    }
+}
